cac459e700391c8c5511dc7b8ce3c63e22f0eaaa
serjioteh <sergei.gb@mail.ru>
2017-10-17 22:09:35 +0300

    remove warning on train (in loss function)

--- ptsemseg/loss.py
+++ ptsemseg/loss.py
@@ -8,7 +8,8 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     n, c, h, w = input.size()
     log_p = F.log_softmax(input)
     log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
-    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    # log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0] # warning
+    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
     log_p = log_p.view(-1, c)
 
     mask = target >= 0
